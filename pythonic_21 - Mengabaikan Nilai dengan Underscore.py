585fa7ef2ec3283058686f2b78597bc032f858ba
thomasbrian4545 <thomasbrian4545@gmail.com>
2024-04-22 23:39:47 +0700

    Create pythonic_21 - Mengabaikan Nilai dengan Underscore.py

---
+++ pythonic_21 - Mengabaikan Nilai dengan Underscore.py
@@ -0,0 +1,8 @@
+for _ in range(3):
+    print("Belajar pemrograman python")
+
+listku = [10, 20, 30, 40, 50]
+a, b, *_ = listku
+print(f"Isi listku = {listku}")
+print(f"Isi a = {a}")
+print(f"Isi b = {b}")
